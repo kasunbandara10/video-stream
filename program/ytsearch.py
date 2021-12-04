b527d0ca9ac5542f2439afadb88812fd7f6f1345
kasunbandara10 <93766168+kasunbandara10@users.noreply.github.com>
2021-12-05 04:38:04 +0530

    Update ytsearch.py

--- program/ytsearch.py
+++ program/ytsearch.py
@@ -34,7 +34,7 @@ async def ytsearch(_, message: Message):
             await message.reply_text("/search **needs an argument !**")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("🔎 **Searching...**")
+        m = await message.reply_text()
         results = YoutubeSearch(query, max_results=5).to_dict()
         i = 0
         text = ""
@@ -42,8 +42,7 @@ async def ytsearch(_, message: Message):
             text += f"🏷 **Name:** __{results[i]['title']}__\n"
             text += f"⏱ **Duration:** `{results[i]['duration']}`\n"
             text += f"👀 **Views:** `{results[i]['views']}`\n"
-            text += f"📣 **Channel:** {results[i]['channel']}\n"
-            text += f"🔗: https://www.youtube.com{results[i]['url_suffix']}\n\n"
+            text += f"📣 **Channel:** {results[i]['channel']}\n\n"
             i += 1
         await m.edit(text, reply_markup=keyboard, disable_web_page_preview=True)
     except Exception as e:
